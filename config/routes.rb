bcb10744d69cf2cfed094952c7a1fa7b36001fb7
Chris Tri <christophertri90@gmail.com>
2019-10-28 10:23:19 +1100

    add pages controller and index which is root for home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # pages/index is the home page.
+  root "pages#index"
+  get 'pages/index'
 end
