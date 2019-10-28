6aa2f45e15380776e0abbb64658630b8ce03a3e4
Chris Tri <christophertri90@gmail.com>
2019-10-28 11:37:49 +1100

    added a navbar, bootstrap styling and changed login path for navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+
+  # Giving sign in page its own path
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+  end
   devise_for :users
   # pages/index is the home page.
   root "pages#index"
   get 'pages/index'
+
+
 end
