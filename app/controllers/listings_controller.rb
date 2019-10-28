d7e947198bb7001c616128542c40cdf77ee707ff
Chris Tri <christophertri90@gmail.com>
2019-10-28 20:10:33 +1100

    updated listing controller to include private methods for user listing and set listing

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -4,6 +4,10 @@ class ListingsController < ApplicationController
   def index
     @listings = Listing.all
   end
+  
+  def show
+
+  end
 
   def new
     @listing = Listing.new
@@ -14,16 +18,40 @@ class ListingsController < ApplicationController
     if @listing.errors.any?
       render 'new'
     else
+      flash[:notice] = "Listing Sucessfully Created"
       redirect_to root_path
     end
   end
 
-  def show
+  def edit
 
   end
   
+  def update
+
+  end
+
+  
 
   private 
+
+  def set_user_listing
+    if current_user.role?
+      set_listing
+    elsif @listing = current_user.listings.find_by_id(params[:id])
+      if @listing == nil
+        redirect_to listings_path
+      else
+        if @listing.price = nil
+          @listing.price = 0
+        end
+      end
+    end
+  end
+
+  def set_listing
+    @listing = Listing.find(params[:id])
+  end
 
   def listing_params
     params.require(:listing).permit(:title,:posted_date,:price,:description,:sold,:condition_id,:user_id,:picture,category_attributes:[:id,:name,:_destroy])
