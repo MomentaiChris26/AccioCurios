6673a420d98860db6b9eaea7f0f978d9ed5e2745
Chris Tri <christophertri90@gmail.com>
2019-10-28 14:25:40 +1100

    added seeding to test out models and associations

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,4 +2,5 @@ class Listing < ApplicationRecord
   belongs_to :condition
   belongs_to :category
   belongs_to :user
+  enum sold: { available: 0, sold: 1 }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,52 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# Creates an admin account for testing purposes
+def seed_admin
+  User.create(
+    id: 1,
+    username: "admin",
+    email: "admin@test.com",
+    password: '123456',
+    password_confirmation: '123456',
+    admin: true,
+  )
+end
+
+# Creates a user account for testing purposes
+def seed_test_user
+  User.create(
+    id: 2,
+    username: "user",
+    email: "test@test.com",
+    password: '123456',
+    password_confirmation: '123456',
+    admin: false,
+  )
+end
+
+# Creates template for product conditions to be used
+def seed_conditions
+  template_conditions = ["New","Used"]
+  template_conditions.each do |condition|
+    Condition.create(status: condition)
+  end
+end
+
+# Creates template categories for testing purposes
+def seed_categories
+  template_categories = ["Toys","Cards","Video Games","Misc"]
+
+  template_categories.each do |category|
+    Category.create(name: category)
+  end
+
+end
+
+
+# Runs methods above 
+seed_admin
+seed_test_user
+seed_conditions
