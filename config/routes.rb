d99befc86d08339398bf05b8c188da4507745f74
Chris Tri <christophertri90@gmail.com>
2019-10-30 11:32:29 +1100

    updated code to resize search bar

--- config/routes.rb
+++ config/routes.rb
@@ -11,16 +11,8 @@ Rails.application.routes.draw do
   # Routing for listings pages
   get 'listings', to: 'listings#index', as: "listings"
   # CRUD for Listings
-  post 'listings', to: 'listings#create'
-  get 'listings/new', to: 'listings#new', as: "new_listing"
-  get "/listings/:id", to: "listings#show", as: "listing"
-  put "/listings/:id", to: "listings#update"
-  patch "/listings/:id", to: "listings#update"
-  delete "/listings/:id", to: "listings#destroy"
-  get "/listings/:id/edit", to: "listings#edit", as: "edit_listing"
+  resources :listings
   get "/admin_dashboard", to: 'listings#admin_dashboard', as: "admin"
-
-
   # CRUD for Conditions
   resources :conditions
 
