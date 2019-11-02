86ecd27926c0de7b8ea9f7d9a20053d85a1ebcd7
Chris Tri <christophertri90@gmail.com>
2019-11-02 21:13:35 +1100

    changed permission for attempting to enter user dashboard without signing in

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,5 @@
 class PagesController < ApplicationController
-  authorize_resource :class => false
-  skip_authorize_resource :only => :index
+  before_action :login_check, only: [:user_dashboard]
   
   def index
     @q = helpers.listing_search
@@ -8,6 +7,17 @@ class PagesController < ApplicationController
 
   def user_dashboard
     
+  end
+
+  private
+
+  def login_check
+    if user_signed_in? == false
+      flash[:alert] = "Please Login To View Dashboard"
+      redirect_to login_path
+    else
+
+    end
   end
 
 
