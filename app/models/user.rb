79b35b725fe90033d65643cb359374d6f9e0c565
Chris Tri <christophertri90@gmail.com>
2019-11-04 13:31:10 +1100

    fixed up issue with devise and problem with unpermitted data

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :listings, dependent: :destroy
   has_many :comments, dependent: :destroy
+  validates :username, :email, uniqueness: true
 end
