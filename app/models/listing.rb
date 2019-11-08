aa43eccb230a76c0cec6a16d7ce40923f9c05dc1
Chris Tri <christophertri90@gmail.com>
2019-11-08 11:22:26 +1100

    updated README

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,7 +2,7 @@ class Listing < ApplicationRecord
   # validations for creating or editing listings
   validates :posted_date, :description, presence: true
   validates :title, uniqueness: true, length: { minimum: 2 }, presence: true
-  validates :price, numericality: { only_float: true }, presence: true
+  validates :price, :format => { :with => /\A\d+(?:\.\d{0,2})?\z/, message: "must be numercial and only contain two decimal points" }, numericality: { only_float: true }, presence: true
   validates_associated :comments
   
   belongs_to :condition
@@ -11,8 +11,7 @@ class Listing < ApplicationRecord
 
   has_one_attached :picture, dependent: :destroy
   has_many :comments, dependent: :destroy
-  has_many :purchase_histories, dependent: nullify
-  has_many :users, :through 
+  has_many :purchase_histories
 
   enum sold: { available: 0, sold: 1 }
   accepts_nested_attributes_for :category, reject_if: :all_blank, allow_destroy: true
