89378b85e9f4ce42de87921f9cbe4a1b31bc1c37
Chris Tri <45407977+MomentaiChris26@users.noreply.github.com>
2025-05-31 19:35:58 +1000

    Update AuthContext.js

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useContext, useEffect } from 'axios';
+import React, { createContext, useState, useContext, useEffect } from 'react';
 import axios from 'axios';
 
 const AuthContext = createContext(null);
